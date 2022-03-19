c88d142ef37062bbb25cd3813d39b418eb6e1f0c
Chris Bain <baincd@users.noreply.github.com>
2022-03-19 12:03:13 -0400

    Auto Fold command folds line 2

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,27 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	
-	let disposable = vscode.commands.registerCommand('custom-auto-fold.autoFold', () => {
-		vscode.window.showInformationMessage('Hello World from Custom Auto Fold!');
+	let disposable = vscode.commands.registerTextEditorCommand('custom-auto-fold.autoFold', (textEditor) => {
+		autoFold(textEditor);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {} 
+
+async function autoFold(textEditor: vscode.TextEditor) {
+	const origSelections = textEditor.selections;
+
+	textEditor.selections = [new vscode.Selection(2,0,2,0)];
+	await timeout(10);
+	await vscode.commands.executeCommand("editor.fold");
+
+	textEditor.selections = origSelections;
+}
+
+async function timeout(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
